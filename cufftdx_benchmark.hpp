3b28f00c756931de69a4eb61606e52140281c965
sangmin7b <sangmin7b@gmail.com>
2025-11-11 22:10:48 +0900

    fix: smem benchmark configs

--- cufftdx_benchmark.hpp
+++ cufftdx_benchmark.hpp
@@ -728,22 +728,22 @@ float benchmark_cufftdx_1d_batch(int batch, bool smem = false, bool e2e = false,
       min_time = std::min(
           min_time, benchmark_cufftdx_1d_batch_impl<N, T, 16, 8, SMNUM, false>(
                         batch, comp_repeats, eval_repeats));
+      min_time = std::min(min_time,
+                          benchmark_cufftdx_1d_batch_thread_impl<N, T, 16, SMNUM>(
+                              batch, comp_repeats, eval_repeats));
+      min_time = std::min(min_time,
+                          benchmark_cufftdx_1d_batch_thread_impl<N, T, 32, SMNUM>(
+                              batch, comp_repeats, eval_repeats));
+      min_time = std::min(min_time,
+                          benchmark_cufftdx_1d_batch_thread_impl<N, T, 64, SMNUM>(
+                              batch, comp_repeats, eval_repeats));
+      min_time = std::min(
+          min_time, benchmark_cufftdx_1d_batch_thread_impl<N, T, 128, SMNUM>(
+                        batch, comp_repeats, eval_repeats));
+      min_time = std::min(
+          min_time, benchmark_cufftdx_1d_batch_thread_impl<N, T, 256, SMNUM>(
+                        batch, comp_repeats, eval_repeats));                        
     }
-    min_time = std::min(min_time,
-                        benchmark_cufftdx_1d_batch_thread_impl<N, T, 16, SMNUM>(
-                            batch, comp_repeats, eval_repeats));
-    min_time = std::min(min_time,
-                        benchmark_cufftdx_1d_batch_thread_impl<N, T, 32, SMNUM>(
-                            batch, comp_repeats, eval_repeats));
-    min_time = std::min(min_time,
-                        benchmark_cufftdx_1d_batch_thread_impl<N, T, 64, SMNUM>(
-                            batch, comp_repeats, eval_repeats));
-    min_time = std::min(
-        min_time, benchmark_cufftdx_1d_batch_thread_impl<N, T, 128, SMNUM>(
-                      batch, comp_repeats, eval_repeats));
-    min_time = std::min(
-        min_time, benchmark_cufftdx_1d_batch_thread_impl<N, T, 256, SMNUM>(
-                      batch, comp_repeats, eval_repeats));
   }
   if constexpr (N == 256 && std::is_same_v<T, __half2>) {
     // smem
